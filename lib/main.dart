42077fa5887d6d3a7e5a303b394b45d6aff16c8a
Rafael <41241789@mackenzista.com.br>
2021-10-03 16:07:33 -0300

    bump exercise

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -8,9 +10,66 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Keys(),
         ),
       ),
     );
+  }
+}
+
+class Keys extends StatefulWidget {
+  // const ({Key? key}) : super(key: key);
+
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<Keys> {
+  List<Widget> buildKey() {
+    return [
+      KeyButton(1, Colors.red),
+      KeyButton(2, Colors.orange),
+      KeyButton(3, Colors.yellow),
+      KeyButton(4, Colors.green),
+      KeyButton(5, Colors.green.shade700),
+      KeyButton(6, Colors.blue),
+      KeyButton(7, Colors.purple),
+    ].toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: buildKey(),
+    );
+  }
+}
+
+class KeyButton extends StatelessWidget {
+  // const ({Key? key}) : super(key: key);
+
+  KeyButton(this.index, this.color);
+
+  final int index;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: FlatButton(
+          color: color,
+          onPressed: () {
+            this.playSound(index);
+          }),
+    );
+  }
+
+  void playSound(int keyValue) {
+    if (keyValue == null) {
+      return;
+    }
+    final player = new AudioCache();
+    player.play('note$keyValue.wav');
   }
 }
